f9a8a8690350bc5cefdbb2bb462d45df0530921a
ALAUSA KHALIL <alausakhalil@gmail.com>
2024-09-18 14:05:29 +0100

    first commit

---
+++ src/componenets/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import '../App.css'
+import HomePage from '../pages/HomePage'
+import Shop from '../pages/Shop'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter basename='/Eeshaa/'>
+      <Routes>
+        <Route index element={<HomePage/>}/>
+        <Route path='/home' element={<HomePage/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
